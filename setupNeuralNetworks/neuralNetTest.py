a20288b9fe4bf19d86c039223ee4df9622e264fb
Karthik Singireddy <skarthikreddy189@gmail.com>
2020-09-08 14:17:26 -0700

    s

---
+++ setupNeuralNetworks/neuralNetTest.py
@@ -0,0 +1,8 @@
+import numpy as np
+import mnist
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+
+print('hello')
